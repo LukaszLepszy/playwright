483594723aa2946a91f3aa0a6c104ee0957612ea
Lukasz_Lepszy <lepszylukasz@gmail.com>
2022-11-02 12:31:55 +0100

    Edited product list page

--- POM/products_list/product_list_locators.py
+++ POM/products_list/product_list_locators.py
@@ -1,1 +1,5 @@
 class ProductListLocators:
+
+    def get_value(self):
+        x = 1
+        return x
--- POM/products_list/product_list_page.py
+++ POM/products_list/product_list_page.py
@@ -2,3 +2,7 @@ from POM.base_page import BasePage
 
 
 class ProductListPage(BasePage):
+
+    def get_string(self):
+        z = "ABC"
+        return z
