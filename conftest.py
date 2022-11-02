38c3e27a9c3235a9fa7ad96bc42142f7f43b2a47
Lukasz_Lepszy <lepszylukasz@gmail.com>
2022-11-02 12:58:36 +0100

    Refactor credencials and modified set_up method

--- conftest.py
+++ conftest.py
@@ -2,9 +2,9 @@ import playwright
 import pytest
 from playwright.sync_api import Playwright
 
-@pytest.fixture(scope='function')
-def set_up(playwright:Playwright):
-    browser = playwright.chromium.launch(headless=False)
+@pytest.fixture(scope='session')
+def set_up(browser):
+    # browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://www.saucedemo.com/")
--- credencials.py
+++ credencials.py
@@ -3,6 +3,6 @@ credencials = {
                 "ligin_1": "standard_user",
                 "ligin_2": "problem_user",
                 "ligin_3": "performance_glitch_user",
-                "ligin_4": "standard_user",
+                "ligin_4": "locked_out_user",
                 "password": "secret_sauce"
 }
